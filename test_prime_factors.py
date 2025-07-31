698a57f8da17a96f4e9495bb99caaf14e04a7bd5
BaeJoungEun <wjddms5810@naver.com>
2025-07-31 12:32:45 +0900

    [feature] 34

--- test_prime_factors.py
+++ test_prime_factors.py
@@ -9,3 +9,7 @@ def test_prime_factor_of_1():
 def test_prime_factor_of_2():
     prime_factor = PrimeFactor()
     assert prime_factor.of(2) == [2]
+
+def test_prime_factor_of_3():
+    prime_factor = PrimeFactor()
+    assert prime_factor.of(3) == [3]
