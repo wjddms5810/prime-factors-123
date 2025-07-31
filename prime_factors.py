7dc6471cfb7d46cdaee802fed080696b1cdb5b4d
BaeJoungEun <wjddms5810@naver.com>
2025-07-31 12:42:29 +0900

    [refactor] 46

--- prime_factors.py
+++ prime_factors.py
@@ -2,13 +2,18 @@ class PrimeFactor:
     def of(self, number):
         factors = []
         if number > 1:
-            if number ==4:
-                while number %2 == 0:
-                    factors.append(2)
-                    number //= 2
-            elif number ==4:
-                factors.append(2)
-                factors.append(3)
+            divisor = 2
+            if number == 4:
+                while number % divisor == 0:
+                    factors.append(divisor)
+                    number //= divisor
+            elif number == 6:
+                divisor = 2
+                while number > 1:
+                    while number % divisor == 0:
+                        factors.append(divisor)
+                        number //= divisor
+                    divisor += 1
             else:
                 factors.append(number)
         return factors
--- test_prime_factors.py
+++ test_prime_factors.py
@@ -10,14 +10,17 @@ def test_prime_factor_of_2():
     prime_factor = PrimeFactor()
     assert prime_factor.of(2) == [2]
 
+
 def test_prime_factor_of_3():
     prime_factor = PrimeFactor()
     assert prime_factor.of(3) == [3]
 
+
 def test_prime_factor_of_4():
     prime_factor = PrimeFactor()
-    assert prime_factor.of(4) == [2,2]
+    assert prime_factor.of(4) == [2, 2]
+
 
 def test_prime_factor_of_6():
     prime_factor = PrimeFactor()
-    assert prime_factor.of(4) == [2,3]
+    assert prime_factor.of(6) == [2, 3]
